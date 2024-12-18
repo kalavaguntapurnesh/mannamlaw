79f69c97e189eef6d5e86f7dbe5686d086a404e8
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-12-18 12:58:19 +0530

    First Commit

--- src/pages/WhyUsPage.jsx
+++ src/pages/WhyUsPage.jsx
@@ -156,11 +156,97 @@ const WhyUsPage = () => {
         </div>
       </div>
 
-      <div>
+      <div className="pt-16 pb-12">
         <div className="relative">
           <div className="w-full">
-            <div className="w-full mx-auto max-w-[1400px] pb-16">
-              <div className="p-4"></div>
+            <div className="w-full mx-auto max-w-[1400px]">
+              <div className="p-4">
+                <div className="space-y-3">
+                  <div className="flex items-center text-center justify-center ">
+                    <div className="h-4 w-1 bg-mainColor"></div>
+
+                    <p className="ml-2 lg:text-lg font-semibold text-mainColor">
+                      Contact Us
+                    </p>
+                  </div>
+
+                  <div className="text-center mt-4">
+                    <h3 className="lg:text-4xl text-2xl font-bold text-headingColor">
+                      Our Prime Locations
+                    </h3>
+                  </div>
+
+                  <div className="text-gray-600 text-center">
+                    <p>
+                      Discover our presence at these convenient locations,
+                      offering easy access to our services and welcoming
+                      environments designed for you.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-6 pt-8 max-w-full">
+                  <div className="flex md:justify-start justify-center">
+                    <div className="w-full bg-white rounded p-6 transition-transform duration-300 lg:hover:-translate-y-2">
+                      <div className="space-y-4">
+                        <div className="flex justify-center items-center">
+                          <img
+                            src="https://images.pexels.com/photos/134643/pexels-photo-134643.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                            alt="services_five"
+                            className="w-full h-60"
+                          />
+                        </div>
+
+                        <div className="flex justify-center items-center">
+                          <h1 className="text-2xl text-headingColor font-bold text-center">
+                            Atlanta, Georgia
+                          </h1>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex md:justify-start justify-center">
+                    <div className="w-full bg-white rounded p-6 transition-transform duration-300 lg:hover:-translate-y-2">
+                      <div className="space-y-4">
+                        <div className="flex justify-center items-center">
+                          <img
+                            src="https://images.pexels.com/photos/2874064/pexels-photo-2874064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                            alt="services_five"
+                            className="w-full h-60"
+                          />
+                        </div>
+
+                        <div className="flex justify-center items-center">
+                          <h1 className="text-2xl text-headingColor font-bold text-center">
+                            Chattanooga, Tennessee
+                          </h1>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex md:justify-start justify-center">
+                    <div className="w-full bg-white rounded p-6 transition-transform duration-300 lg:hover:-translate-y-2">
+                      <div className="space-y-4">
+                        <div className="flex justify-center items-center">
+                          <img
+                            src="https://images.pexels.com/photos/2815184/pexels-photo-2815184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                            alt="services_five"
+                            className="w-full h-60"
+                          />
+                        </div>
+
+                        <div className="flex justify-center items-center">
+                          <h1 className="text-2xl text-headingColor font-bold text-center">
+                            Suwanee, Georgia
+                          </h1>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
