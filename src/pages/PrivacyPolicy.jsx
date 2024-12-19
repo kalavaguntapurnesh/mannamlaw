996cd98f674ab720976d07a560bbc505905083a8
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-12-19 13:59:07 +0530

    First Commit

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -2,11 +2,41 @@ import React from "react";
 import Footer from "../components/Footer";
 import SecNavbar from "./../components/SecNavbar";
 import ScrollToTop from "../components/ScrollToTop";
+import { ImPower } from "react-icons/im";
+import { FaFlag } from "react-icons/fa6";
 
 const PrivacyPolicy = () => {
   return (
     <>
       <SecNavbar />
+
+      <div className="lg:pt-[72px] pt-[80px]">
+        <div className="relative">
+          <div className="w-full">
+            <div className="w-full">
+              <div className="bg-black relative">
+                <img
+                  src="https://images.pexels.com/photos/60504/security-protection-anti-virus-software-60504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                  alt="Justice law case hearing"
+                  className="relative w-full lg:h-[480px] object-cover bg-opacity-10"
+                />
+              </div>
+              <div className="absolute inset-0 flex flex-col items-center justify-center bg-black text-white bg-opacity-50">
+                <h1 className=" lg:text-5xl lg:block hidden uppercase font-semibold text-center px-4 py-2 rounded">
+                  Mannam Law & Associate's Privacy Policy
+                </h1>
+
+                <h1 className="text-4xl lg:hidden block uppercase font-semibold text-center px-4 py-2 rounded">
+                  Our Privacy Policy
+                </h1>
+                <p className="lg:text-xl text-center">
+                  Protecting your information and ensuring transparency.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <ScrollToTop />
       <Footer />
     </>
